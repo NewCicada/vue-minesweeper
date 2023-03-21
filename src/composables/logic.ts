1af146f14a8f949522d55f63735ee6747dc3b923
NewCicada <1547598968@qq.com>
2023-03-21 22:36:32 +0800

    feat: alert

--- src/composables/logic.ts
+++ src/composables/logic.ts
@@ -140,9 +140,11 @@ export class GamePlay {
       if (blocks.some(block => block.flagged && !block.mine)) {
         this.state.value.gameState = 'lost'
         this.showAllMine()
+        alert('lost')
       }
       else {
         this.state.value.gameState = 'won'
+        alert('won')
       }
     }
   }
