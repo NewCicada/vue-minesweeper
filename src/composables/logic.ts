048d40d3958234887fc7e1a21580b6e335245a52
NewCicada <1547598968@qq.com>
2023-03-23 12:01:07 +0800

    chore: setup random seed

--- src/composables/logic.ts
+++ src/composables/logic.ts
@@ -1,3 +1,5 @@
+import type { Ref } from 'vue'
+// import type createSeedrandom from 'seedrandom'
 import type { BlockState } from '~/types'
 
 const directions = [
@@ -21,6 +23,7 @@ interface GameState {
 }
 export class GamePlay {
   state = ref() as Ref<GameState>
+  // random: ReturnType<typeof createSeedrandom>
 
   constructor(
     public width: number,
@@ -64,12 +67,12 @@ export class GamePlay {
     }
   }
 
-  random(min: number, max: number) {
+  randomRange(min: number, max: number) {
     return Math.random() * (max - min) + min
   }
 
   randomInt(min: number, max: number) {
-    return Math.round(this.random(min, max))
+    return Math.round(this.randomRange(min, max))
   }
 
   generateMines(state: BlockState[][], initial: BlockState) {
